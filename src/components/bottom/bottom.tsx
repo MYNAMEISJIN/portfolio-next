cb3f8f9816773a4692baada7192cfc4895a6ce37
Jin <hbay11@naver.com>
2024-03-21 00:40:31 +0900

    CV commit

--- src/components/bottom/bottom.tsx
+++ src/components/bottom/bottom.tsx
@@ -4,7 +4,7 @@ const Bottom = () => {
   return (
     <div className="h-96 flex flex-col items-center justify-between pb-4">
       <div className="h-full flex items-center flex-col justify-center gap-10">
-        <Link href={"https://www.google.com"} target="_blank" className="border-4 rounded font-bold text-3xl flex flex-col items-center justify-center h-16 w-60
+        <Link href={"https://jin-cv.tiiny.site/"} target="_blank" className="border-4 rounded font-bold text-3xl flex flex-col items-center justify-center h-16 w-60
        hover:border-orange-600 hover:text-orange-500 hover:bg-white transition-all">
           CV
         </Link>
