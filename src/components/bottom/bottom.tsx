35d6fc90f53acbed1c6fba399870762551d490a1
MYNAMEISJIN <89293441+MYNAMEISJIN@users.noreply.github.com>
2025-09-09 13:22:53 +0100

    Update bottom.tsx

--- src/components/bottom/bottom.tsx
+++ src/components/bottom/bottom.tsx
@@ -8,7 +8,7 @@ const Bottom = () => {
     <div className="h-96 flex flex-col items-center justify-between pb-4">
       <div className="h-full flex items-center flex-col justify-center gap-10">
         
-          <Link href={"https://jinnn-cv.tiiny.site/"} target="_blank" className="border-4 rounded font-bold text-3xl flex flex-col items-center justify-center h-16 w-60
+          <Link href={"https://jin-cv.tiiny.site"} target="_blank" className="border-4 rounded font-bold text-3xl flex flex-col items-center justify-center h-16 w-60
        hover:border-orange-600 hover:text-orange-500 hover:bg-white transition-all">
             CV
           </Link>
